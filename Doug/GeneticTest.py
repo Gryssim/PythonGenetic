da6544964a8bd268e4b1c9ab59d53283a7916792
dgplock <dgplock@gmail.com>
2017-05-02 14:05:20 -0600

    Changed folder names

--- Doug/GeneticTest.py
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-
-=======
-Testing...
-I Like your TeSt :) --by joey
->>>>>>> f2a2356d82bd66c265c10daf2fbcffbb5b3b013c
--- Doug/WordFinderQT/mapProp.py
+++
@@ -1,3 +0,0 @@
-
-def mapProp(value, istart, istop, ostart, ostop):
-    return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
